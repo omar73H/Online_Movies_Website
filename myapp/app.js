800d8845721eb3884b786a8b9a7b950337389694
aman <amanrafat299@yahoo.com>
2019-09-29 19:30:04 +0200

    changes in first tut

--- myapp/app.js
+++ myapp/app.js
@@ -1,11 +1,6 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var fs = require('fs');
 
 var app = express();
 
@@ -13,29 +8,39 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+//the actual code for the project.
+//------------------------------------------------------------------------------------------------------------------
+app.get('/', function(req, res) {
+  res.render('index', { title: 'Express' });
+});
+//------------------------------------------------------------------------------------------------------------------
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+// examples for getting pages by requesting the server (app).-----------------------------------
+app.get('/temp', function(req, res) {
+  res.render('temp', { title: 'Express' });
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.get('/temp2', function(req, res) {
+  res.send("Aman Allah");
 });
-app.listen(3000)
+
+app.post('/', function(req, res) {
+  res.send("AAAAAAAAAAAAA");
+});
+
+/*var myObj = {name : "Aman" , age : 21};
+console.log(myObj);
+var j = JSON.stringify(myObj);
+console.log(j);
+fs.writeFileSync("users.json",j);
+var r = fs.readFileSync("users.json");
+var obj = JSON.parse(r);
+console.log(obj);*/
+//end of the examples.---------------------------------------------------------------------------
+
+app.listen(3000);
 module.exports = app;
